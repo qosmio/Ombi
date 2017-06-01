c3b3b0bcce81e7c7e7ebef49acbcefde40771fdc
xbmc <xbmc@htpc>
2017-06-01 00:25:39 -0500

    Updated with local appveyor

--- src/Build/publish.bat
+++ src/Build/publish.bat
@@ -3,7 +3,7 @@ cd ..
 dotnet restore
 dotnet publish -c Release /p:AppRuntimeIdentifier=win10-x64
 dotnet publish -c Release /p:AppRuntimeIdentifier=osx.10.12-x64
-dotnet publish -c Release /p:AppRuntimeIdentifier=ubuntu.16.10-x64
+dotnet publish -c Release /p:AppRuntimeIdentifier=ubuntu.16.04-x64
 dotnet publish -c Release /p:AppRuntimeIdentifier=debian.8-x64
 
 exit
